da137f7a1190fcf45d0d4457e1be0227caa8957d
Aurora Duncan <aurorabrynn@gmail.com>
2021-04-01 15:28:42 -0700

    fixed colors, setting up save buttons

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,23 +1,92 @@
 var description = $('.description');
-var hour = $('.hour');
-var hourArray = ["09:00:00", "10:00:00", "11:00:00", "12:00:00", "13:00:00", "14:00:00", "15:00:00", "16:00:00", "17:00:00"];
+var nine = $('#nine');
+var ten = $('#ten');
+var eleven = $('#eleven');
+var twelve = $('#twelve');
+var thirteen = $('#thirteen');
+var fourteen = $('#fourteen');
+var fifteen = $('#fifteen');
+var sixteen = $('#sixteen');
+var seventeen = $('#seventeen');
 var save = $('.saveBtn');
 
 function timeColors() {
-    hourArray.forEach(function (hourArray, i) {
-        if (moment().isAfter(hourArray[i], "hour")) {
-            description.addClass('past');
-        } else if (moment().isBefore(hourArray[i], "hour")) {
-            description.addClass('future');
-        } else {
-            description.addClass('present');
-        }
-    })
+    if (moment().format("HH") > moment("09", "HH").format("HH")) {
+        nine.addClass('past');
+    } else if (moment().format("HH") < moment("09", "HH").format("HH")) {
+        nine.addClass('future');
+    } else {
+        nine.addClass('present');
+    }
+
+    if (moment().format("HH") > moment("10", "HH").format("HH")) {
+        ten.addClass('past');
+    } else if (moment().format("HH") < moment("10", "HH").format("HH")) {
+        ten.addClass('future');
+    } else {
+        ten.addClass('present');
+    }
+
+    if (moment().format("HH") > moment("11", "HH").format("HH")) {
+        eleven.addClass('past');
+    } else if (moment().format("HH") < moment("11", "HH").format("HH")) {
+        eleven.addClass('future');
+    } else {
+        eleven.addClass('present');
+    }
+
+    if (moment().format("HH") > moment("12", "HH").format("HH")) {
+        twelve.addClass('past');
+    } else if (moment().format("HH") < moment("12", "HH").format("HH")) {
+        twelve.addClass('future');
+    } else {
+        twelve.addClass('present');
+    }
+
+    if (moment().format("HH") > moment("13", "HH").format("HH")) {
+        thirteen.addClass('past');
+    } else if (moment().format("HH") < moment("13", "HH").format("HH")) {
+        thirteen.addClass('future');
+    } else {
+        thirteen.addClass('present');
+    }
+
+    if (moment().format("HH") > moment("14", "HH").format("HH")) {
+        fourteen.addClass('past');
+    } else if (moment().format("HH") < moment("14", "HH").format("HH")) {
+        fourteen.addClass('future');
+    } else {
+        fourteen.addClass('present');
+    }
+
+    if (moment().format("HH") > moment("15", "HH").format("HH")) {
+        fifteen.addClass('past');
+    } else if (moment().format("HH") < moment("15", "HH").format("HH")) {
+        fifteen.addClass('future');
+    } else {
+        fifteen.addClass('present');
+    }
+
+    if (moment().format("HH") > moment("16", "HH").format("HH")) {
+        sixteen.addClass('past');
+    } else if (moment().format("HH") < moment("16", "HH").format("HH")) {
+        sixteen.addClass('future');
+    } else {
+        sixteen.addClass('present');
+    }
+
+    if (moment().format("HH") > moment("17", "HH").format("HH")) {
+        seventeen.addClass('past');
+    } else if (moment().format("HH") < moment("17", "HH").format("HH")) {
+        seventeen.addClass('future');
+    } else {
+        seventeen.addClass('present');
+    }
 }
 
-function saving() {
-    localStorage.setItem("description", description.value);
-}
+
+save.on("click", function () {
+    localStorage.setItem("task", description.value);
+});
 
 timeColors();
-save.addEventListener("click", saving());
